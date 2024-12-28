84092b045e4a1d6da5931b08a909802b66e8f94e
vilanz <vs.lanzarini@gmail.com>
2024-12-28 17:40:19 -0300

    redirect to smiley, no direct server

--- lib/ee_web_archive_web/controllers/smiley_controller.ex
+++ lib/ee_web_archive_web/controllers/smiley_controller.ex
@@ -4,19 +4,12 @@ defmodule EEWebArchiveWeb.SmileyController do
 
   def by_player(conn, %{"player_id" => player_id}) do
     smiley_id = Smileys.get_player_smiley_id(player_id)
-    path = smiley_path(smiley_id)
-
-    if File.exists?(path) do
-      conn
+    conn
       |> put_resp_header("cache-control", "private, max-age=10080")
-      |> send_file(200, path)
-    else
-      put_status(conn, 404)
-    end
+      |> redirect(to: smiley_path(conn, smiley_id))
   end
 
-  def smiley_path(smiley_id) do
-    smiley_filename = "#{smiley_id}.png" # ~p sigil does not allow anything after interpolation
-    ~p"/images/smileys/#{smiley_filename}"
+  def smiley_path(conn, smiley_id) do
+    static_path(conn, "/images/smileys/#{smiley_id}.png")
   end
 end
